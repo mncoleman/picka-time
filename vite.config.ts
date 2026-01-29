a41c61539412663ec1fd7a27aa345d9623b94a4e
Matthew Coleman <124199027+mncoleman@users.noreply.github.com>
2026-01-28 18:35:50 -0700

    update for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/picka-time/", // GitHub Pages compatible base path
+  base: "/Secret-Exchange/", // GitHub Pages compatible base path
   server: {
     host: "::",
     port: 8080,
